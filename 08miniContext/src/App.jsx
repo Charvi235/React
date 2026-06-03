92b62eaf5f8f588c25d54433668e70c26cf42963
Charvi235 <charvikhandelwal023@gmail.com>
2026-06-03 14:37:40 +0530

    09themeWitcher

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import UserContextProvider from './Context/UserContextProvider'
 import './App.css'
 import Login from './Components/Login'
-
+import Profile from './Components/Profile'
 function App() {
   const [count, setCount] = useState(0)
   
